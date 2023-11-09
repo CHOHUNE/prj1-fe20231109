02373bf8f520d23aad0aeb04d3a536695f6f0452
CHOHUNE <yuureru@gmail.com>
2023-11-09 16:22:36 +0900

    게시물 보기 화면 정리

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 import axios from "axios";
 
 export function BoardView() {
@@ -20,7 +27,27 @@ export function BoardView() {
 
   return (
     <Box>
-      <h1> 글 보기 </h1>
+      <h1> 글 보기 </h1>ㅑ
+      <FormControl>
+        <FormLabel>
+          제목
+          <Input value={board.title} readOnly />
+        </FormLabel>
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea value={board.content} readOnly>
+          {" "}
+        </Textarea>
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input value={board.writer} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성일시</FormLabel>
+        <Input value={board.inserted} readOnly />
+      </FormControl>
       <p>번호 :{board.id}</p>
       <p>제목: {board.title}</p>
       <p>본문 : {board.content}</p>
