993446225a0b89c2ce95bbda484e6c8b2c2361ac
CHOHUNE <yuureru@gmail.com>
2023-11-09 14:30:34 +0900

    저장 후 home 으로 이동 까지

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import { BoardWrite } from "./page/BoardWrite";
+import { HomeLayout } from "./layout/HomeLayout";
+import { BoardList } from "./page/BoardList";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={"/"} element={<HomeLayout />}>
+      <Route index element={<BoardList />} />
+      <Route path={"write"} element={<BoardWrite />} />
+    </Route>,
+  ),
+);
 
 function App(props) {
   return (
-    <div></div>
+    <div>
+      <RouterProvider router={routes} />
+    </div>
   );
 }
 
