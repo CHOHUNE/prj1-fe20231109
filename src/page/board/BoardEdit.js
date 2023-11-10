e93e459864bfbddf9a2eb443110ef6c3e0bf5bd3
CHOHUNE <yuureru@gmail.com>
2023-11-10 11:51:04 +0900

    회원가입 폼 작성

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -6,11 +6,13 @@ import {
   Input,
   Spinner,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
+import { logDOM } from "@testing-library/react";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
@@ -18,6 +20,7 @@ export function BoardEdit() {
   // /edit/:id
   const { id } = useParams();
   const navigate = useNavigate();
+  const toast = useToast();
 
   useEffect(() => {
     axios
@@ -29,6 +32,36 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  function handleSubmit() {
+    // 저장 버튼 클릭 시
+    // PUT /api/board/edit
+
+    axios
+      .put("/api/board/edit", board)
+      .then(() => {
+        toast({
+          description: board.id + "번 게시글이 수정되었습니다.",
+          status: "success",
+        });
+
+        navigate("/board/" + id);
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          toast({
+            description: "요청이 잘못되었습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "수정 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => console.log("끝"));
+  }
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
@@ -36,36 +69,39 @@ export function BoardEdit() {
         <FormLabel>제목</FormLabel>
         <Input
           value={board.title}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.title = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
         <Textarea
           value={board.content}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.content = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
         <Input
           value={board.writer}
-          onChange={(e) => {
+          onChange={(e) =>
             updateBoard((draft) => {
               draft.writer = e.target.value;
-            });
-          }}
+            })
+          }
         />
       </FormControl>
-      <Button colorScheme="blue">저장</Button>
+      <Button colorScheme="blue" onClick={handleSubmit}>
+        저장
+      </Button>
+      {/* navigate(-1) : 이전 경로로 이동 */}
       <Button onClick={() => navigate(-1)}>취소</Button>
     </Box>
   );
